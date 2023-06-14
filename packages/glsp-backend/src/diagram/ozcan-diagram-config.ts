4090e1e727dc0cfd700eb817a664ce3652d82df4
Özcan Seker <ozcan@x-breeze.com>
2023-06-14 15:10:27 +0200

    tussentijdse commit

---
+++ packages/glsp-backend/src/diagram/ozcan-diagram-config.ts
@@ -0,0 +1,31 @@
+// Stores the options for the diagram and all the subjects
+
+import { DefaultTypes, DiagramConfiguration, EdgeTypeHint, GModelElementConstructor, ServerLayoutKind, ShapeTypeHint, getDefaultMapping } from "@eclipse-glsp/server-node";
+import { injectable } from 'inversify';
+
+@injectable()
+export class OzcanDiagramConfig implements DiagramConfiguration{    
+    get typeMapping(): Map<string, GModelElementConstructor>{
+        return getDefaultMapping()
+    }
+
+    get shapeTypeHints(): ShapeTypeHint[]{
+        return [{elementTypeId: DefaultTypes.NODE, deletable: true, reparentable: false, repositionable: true, resizable: true}]
+    }
+
+    get edgeTypeHints(): EdgeTypeHint[]{
+        return [{
+            elementTypeId: DefaultTypes.EDGE, 
+            repositionable: true, 
+            routable: true, 
+            deletable: true, 
+            sourceElementTypeIds: [DefaultTypes.NODE], 
+            targetElementTypeIds: [DefaultTypes.NODE]
+        }]
+    }
+
+    layoutKind: ServerLayoutKind.AUTOMATIC;
+
+    needsClientLayout = false;
+    animatedUpdate = true;
+}
